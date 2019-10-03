7cde84ded7f685a71586ebd3e2989b98f0ea5fcf
Polyteh <prevoivan@gmail.com>
2019-10-03 21:50:20 +0300

    Lesson 17, practice, task 1

---
+++ Practice_lesson17/Practice_01/LinkedListGeneric.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_01
+{
+    public class LinkedListGeneric<T>:IEnumerable<T>
+    {
+        private LinkedList<T> myLinkedList;
+        public LinkedListGeneric()
+        {
+            myLinkedList = new LinkedList<T>();
+        }
+        public void AddItem(T newItem, int index)
+        {
+            LinkedListNode<T> insertAfterNode = myLinkedList.First;
+
+            if (myLinkedList.Count==0)
+            {
+                myLinkedList.AddFirst(newItem);               
+            }
+
+            for (int i = 0; (myLinkedList.Count != 0) &&(i <= index)&&(insertAfterNode!=null); i++)
+            {
+               
+                if (i==(index-1))
+                {
+                    myLinkedList.AddAfter(insertAfterNode, newItem);
+                    break;
+                }
+                insertAfterNode = insertAfterNode.Next;
+
+            }
+
+        }
+
+        public void DeleteItem(int index)
+        {
+            LinkedListNode<T> nodeToDelete = myLinkedList.First;
+            for (int i = 0; (i <= index) && (nodeToDelete != null); i++)
+            {
+
+                if (i==index)
+                {
+                    myLinkedList.Remove(nodeToDelete);
+                    break;
+                }
+                nodeToDelete = nodeToDelete.Next;
+            }
+
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return myLinkedList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return myLinkedList.GetEnumerator();
+        }
+    }
+}
---
+++ Practice_lesson17/Practice_01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListGeneric<int> myLinkedList = new LinkedListGeneric<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                myLinkedList.AddItem(i,i);
+            }
+
+            Console.WriteLine("Initial linked list");
+            foreach (var item in myLinkedList)
+            {
+                Console.WriteLine(item);
+            }
+
+            int indexToDelete = 3;
+            Console.WriteLine($"Linked list without element with index {indexToDelete}");
+            myLinkedList.DeleteItem(indexToDelete);
+            foreach (var item in myLinkedList)
+            {
+                Console.WriteLine(item);
+            }
+
+            indexToDelete = 0;
+            Console.WriteLine($"Linked list without element with index {indexToDelete}");
+            myLinkedList.DeleteItem(indexToDelete);
+            foreach (var item in myLinkedList)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
